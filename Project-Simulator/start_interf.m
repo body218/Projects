cbc6129a61360eb49d0e4a2b22f9244a627cee78
body218 <73294510+body218@users.noreply.github.com>
2022-02-16 12:59:17 +0200

    Add files via upload

---
+++ Project-Simulator/start_interf.m
@@ -0,0 +1,13 @@
+clear All;
+close All;
+
+R1=1;
+R2=1;
+f=1000;  %frecventa
+Vin=1;   %Amplitudinea semnalului de intrare
+E=15;    %Alimentare diferentiala     
+tip=1;
+s1=1;
+s2=1;
+interf(Vin,f,R1,R2,E,tip,s1,s2);
+
